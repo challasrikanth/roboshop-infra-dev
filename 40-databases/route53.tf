81b920b3f1cce15627479c44432a440e541bc270
srikanth <csri@nuuday.com>
2026-03-10 17:13:47 +0530

    added route53 record for rabbitmq

--- 40-databases/route53.tf
+++ 40-databases/route53.tf
@@ -21,9 +21,18 @@ resource "aws_route53_record" "redis" {
 
 resource "aws_route53_record" "mysql" {
   zone_id = var.zone_id
-  name    = "redis-${var.environment}.${var.domain_name}"
+  name    = "mysql-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = "1"
   records = [aws_instance.mysql.private_ip]
   allow_overwrite = true
 }
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name = "rabbitmq-${var.environment}.${var.domain_name}"
+  type = "A"
+  ttl = "1"
+  records =  [aws_instance.rabbitmq.private_ip] 
+  allow_overwrite = true
+  }
