568bccca33684685e4b113ae575d9fc2e317404d
srikanth <csri@nuuday.com>
2026-03-18 19:56:06 +0530

    updated

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -1,35 +1,35 @@
 data "aws_ssm_parameter" "bastion_sg_id" {
-    name = "/${var.project}/${var.environment}/bastion"
+    name = "/${var.project}/${var.environment}/bastion_sg_id"
   
 }
 
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
-    name = "/${var.project}/${var.environment}/mongodb"
+    name = "/${var.project}/${var.environment}/mongodb_sg_id"
   
 }
 
 data "aws_ssm_parameter" "redis_sg_id" {
-    name = "/${var.project}/${var.environment}/redis"
+    name = "/${var.project}/${var.environment}/redis_sg_id"
 }
 
 data "aws_ssm_parameter" "catalogue_sg_id" {
-    name = "/${var.project}/${var.environment}/catalogue"
+    name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
 
 data "aws_ssm_parameter" "user_sg_id" {
-    name = "/${var.project}/${var.environment}/user"
+    name = "/${var.project}/${var.environment}/user_sg_id"
 }
 
 data "aws_ssm_parameter" "mysql_sg_id" {
-    name = "/${var.project}/${var.environment}/mysql"
+    name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
 
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
-    name = "/${var.project}/${var.environment}/rabbitmq"
+    name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 }
 
 data "aws_ssm_parameter" "backend_alb_sg_id" {
-    name = "/${var.project}/${var.environment}/backend_alb"
+    name = "/${var.project}/${var.environment}/backend_alb_sg_id"
   
 }
