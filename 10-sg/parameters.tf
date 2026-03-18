b3a5a202302f62187eb79b60c5e922c308a227e7
srikanth <csri@nuuday.com>
2026-03-18 19:55:36 +0530

    updated

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "sg_id" {
 
 count = length(var.sg_name)
 type = "String"
-name = "/${var.project}/${var.environment}/${var.sg_name[count.index]}"
+name = "/${var.project}/${var.environment}/${var.sg_name[count.index]}_sg_id"
 value = module.sg[count.index].sg_id
 
 #.sg_id → fetch sg_id output from each
