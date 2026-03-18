48b19525ddb010bd138a467dc7e38c00858a6250
srikanth <csri@nuuday.com>
2026-03-18 21:58:36 +0530

    update mongodb_code

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -10,5 +10,5 @@ locals {
   private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_ids.value)[0]
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
+#   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
 }
